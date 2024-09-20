4318dcc7aae8f5839121c073f786ad4a2f184f46
KrankVegan <martinezcawork@gmail.com>
2024-09-20 17:50:52 -0500

    started latest products segment

---
+++ components/ProductBox.js
@@ -0,0 +1,5 @@
+export default function ProductBox({_id,title,description,price,images}) {
+    return (
+        <div> {title} </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import Header from "@/components/Header";
 import Featured from "@/components/Featured";
 import {Product} from "@/models/Product";
 import {mongooseConnect} from "@/lib/mongoose";
+import NewProducts from "@/components/NewProduts";
 
-export default function HomePage({product}) {
+export default function HomePage({featuredProduct, newProducts}) {
   return (
     <div>
       <Header />
 
-      <Featured product={product} />
+      <Featured product={featuredProduct} />
+
+      <NewProducts products = {newProducts}> </NewProducts>
     </div>
   );
 }
@@ -16,8 +19,12 @@ export default function HomePage({product}) {
 export async function getServerSideProps() {
   const featuredProductId = '66ec25d7c3b2639aa8a233c3';
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
   return {
-    props: {product: JSON.parse(JSON.stringify(product))},
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
